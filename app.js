bfc7f5f3c330df42d105f02d0eaad1084bdc44a7
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-05 23:03:28 +0700

    Express simple REST

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public"));
 
+app.all("/", (req, res, next) => {
+  console.log("Client hit the server");
+  next();
+});
+
+app.get("/dishes", (req, res) => {
+  res.status(200).send("Getting all dishes...");
+});
+
+app.get("/dishes/:dishId", (req, res) => {
+  res.status(200).send(`Getting dish: ${req.params.dishId}`);
+});
+
+app.post("/dishes", (req, res) => {
+  res.status(200).send("Creating a dish...");
+});
+
+app.put("/dishes/:dishId", (req, res) => {
+  res.status(200).send(`Updating dish: ${req.params.dishId}`);
+});
+
+app.delete("/dishes/:dishId", (req, res) => {
+  res.status(200).send(`Deleting dish: ${req.params.dishId}`);
+});
+
 app.listen(port, hostname, () => {
   console.log(`Server is listening on port ${port}`);
 });
