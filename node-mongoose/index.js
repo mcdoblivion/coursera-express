9f6bedc987a47ac8ad7fdd2c31c012352c882c06
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-08 16:47:36 +0700

    Mongoose part 1

---
+++ node-mongoose/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://localhost:27017/conFusion";
+
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server.");
+
+  var newDish = Dishes({
+    name: "dish 1",
+    description: "Sample description",
+  });
+
+  newDish
+    .save()
+    .then((dish) => {
+      console.log(dish);
+
+      return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+      console.log(dishes);
+
+      return Dishes.remove();
+    })
+    .then(() => {
+      mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
