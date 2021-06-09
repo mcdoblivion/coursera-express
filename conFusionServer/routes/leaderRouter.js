491f08405792447acff7388525cd573fc78d5723
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-10 00:35:51 +0700

    Update leaderRouter to support REST with Mongoose

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,29 +1,87 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Leaders = require("../models/leaders");
+
 const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
+// /leaders/
 leaderRouter
   .route("/")
-  .get((req, res) => {
-    res.status(200).send("Getting all leaders...");
+  .get((req, res, next) => {
+    Leaders.find({})
+      .then(
+        (leaders) => {
+          res.status(200).json(leaders);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
-  .post((req, res) => {
-    res.status(200).send("Creating a leader...");
-  });
-
-leaderRouter
-  .route("/:leaderId")
-  .get((req, res) => {
-    res.status(200).send(`Getting leader: ${req.params.leaderId}`);
+  .post((req, res, next) => {
+    Leaders.create(req.body)
+      .then(
+        (leader) => {
+          console.log("Leader created");
+          res.status(200).json(leader);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
   .put((req, res) => {
-    res.status(200).send(`Updating leader: ${req.params.leaderId}`);
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /leaders/");
   })
   .delete((req, res) => {
-    res.status(200).send(`Deleting leader: ${req.params.leaderId}`);
+    res.statusCode = 403;
+    res.end("DELETE operation not supported on /leaders/");
+  });
+
+// /leaders/:leaderId
+leaderRouter
+  .route("/:leaderId")
+  .get((req, res, next) => {
+    Leaders.findById(req.params.leaderId)
+      .then(
+        (leader) => {
+          res.status(200).json(leader);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end("POST operation not supported on /leaders/:leaderId");
+  })
+  .put((req, res, next) => {
+    Leaders.findByIdAndUpdate(
+      req.params.leaderId,
+      { $set: req.body },
+      { new: this.true }
+    )
+      .then(
+        (leader) => {
+          res.status(200).json(leader);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Leaders.findByIdAndDelete(req.params.leaderId)
+      .then(
+        (response) => {
+          console.log("Leader deleted");
+
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   });
 
 module.exports = leaderRouter;
