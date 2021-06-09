34a630e4004e1afdf90e2c67f8c7c756f6905d14
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-09 19:36:14 +0700

    Express REST API with MongoDB and Mongoose Part 1

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -1,29 +1,88 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+const Dishes = require("../models/dishes");
+
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter
   .route("/")
-  .get((req, res) => {
-    res.status(200).send("Getting all dishes...");
+  .get((req, res, next) => {
+    Dishes.find({})
+      .then(
+        (dishes) => {
+          res.status(200).json(dishes);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
-  .post((req, res) => {
-    res.status(200).send("Creating a dish...");
+  .post((req, res, next) => {
+    Dishes.create(req.body)
+      .then(
+        (dish) => {
+          console.log("Dish created");
+
+          res.status(200).json(dish);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Dishes.remove({})
+      .then(
+        (response) => {
+          console.log("Dishes deleted");
+
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   });
 
 dishRouter
   .route("/:dishId")
-  .get((req, res) => {
-    res.status(200).send(`Getting dish: ${req.params.dishId}`);
+  .get((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+      .then(
+        (dish) => {
+          res.status(200).json(dish);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
-  .put((req, res) => {
-    res.status(200).send(`Updating dish: ${req.params.dishId}`);
+  .put((req, res, next) => {
+    Dishes.findByIdAndUpdate(
+      req.params.dishId,
+      { $set: req.body },
+      { new: true }
+    )
+      .then(
+        (dish) => {
+          res.status(200).json(dish);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
-  .delete((req, res) => {
-    res.status(200).send(`Deleting dish: ${req.params.dishId}`);
+  .delete((req, res, next) => {
+    Dishes.findByIdAndDelete(req.params.dishId)
+      .then(
+        (response) => {
+          console.log("Dishes deleted");
+
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   });
 
 module.exports = dishRouter;
