2f5499bc48ef2d0342a1c17123fa73751f398037
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-10 00:28:18 +0700

    Update promoRouter to support REST with Mongoose

--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,29 +1,95 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Promotions = require("../models/promotions");
+
 const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
 
+// /promotions/
 promoRouter
   .route("/")
-  .get((req, res) => {
-    res.status(200).send("Getting all promotions...");
+  .get((req, res, next) => {
+    Promotions.find({})
+      .then(
+        (promotions) => {
+          res.status(200).json(promotions);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
-  .post((req, res) => {
-    res.status(200).send("Creating a promotion...");
-  });
-
-promoRouter
-  .route("/:promoId")
-  .get((req, res) => {
-    res.status(200).send(`Getting promotion: ${req.params.promoId}`);
+  .post((req, res, next) => {
+    Promotions.create(req.body)
+      .then(
+        (promotion) => {
+          console.log("Promotion created");
+          res.status(200).json(promotion);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
   .put((req, res) => {
-    res.status(200).send(`Updating promotion: ${req.params.promoId}`);
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /promotions/");
   })
-  .delete((req, res) => {
-    res.status(200).send(`Deleting promotion: ${req.params.promoId}`);
+  .delete((req, res, next) => {
+    Promotions.remove({})
+      .then(
+        (response) => {
+          console.log("All promotion deletted");
+
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
+
+// /promotions/:promoId
+promoRouter
+  .route("/:promoId")
+  .get((req, res, next) => {
+    Promotions.findById(req.params.promoId)
+      .then(
+        (promotion) => {
+          res.status(200).json(promotion);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end("POST operation not supported on /promotions/:promoId");
+  })
+  .put((req, res, next) => {
+    Promotions.findByIdAndUpdate(
+      req.params.promoId,
+      { $set: req.body },
+      { new: this.true }
+    )
+      .then(
+        (promotion) => {
+          res.status(200).json(promotion);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promotions.findByIdAndDelete(req.params.promoId)
+      .then(
+        (response) => {
+          console.log("Promotion deleted");
+
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   });
 
 module.exports = promoRouter;
