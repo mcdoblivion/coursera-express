7d9f6881749bcf1a2b62a954e0b81a2eb5171082
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-10 00:54:44 +0700

    Add timestamps to promotion and leader schema

--- conFusionServer/models/leaders.js
+++ conFusionServer/models/leaders.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const leaderSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const leaderSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    designation: {
+      type: String,
+      required: true,
+    },
+    abbr: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
   },
-  image: {
-    type: String,
-    required: true,
-  },
-  designation: {
-    type: String,
-    required: true,
-  },
-  abbr: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 var Leaders = mongoose.model("leaders", leaderSchema);
 
--- conFusionServer/models/promotions.js
+++ conFusionServer/models/promotions.js
@@ -3,33 +3,37 @@ const Schema = mongoose.Schema;
 require("mongoose-currency").loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const promotionSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const promotionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
   },
-  image: {
-    type: String,
-    required: true,
-  },
-  label: {
-    type: String,
-    default: "",
-  },
-  price: {
-    type: Currency,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 var Promotions = mongoose.model("promotions", promotionSchema);
 
