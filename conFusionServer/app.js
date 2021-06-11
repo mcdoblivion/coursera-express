ed6b4f0b174dc30c5d699cca7a24d8ab4307cc3a
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-11 22:42:13 +0700

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -18,6 +18,16 @@ const leaderRouter = require("./routes/leaderRouter");
 
 var app = express();
 
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  if (req.secure) next();
+  else
+    res.redirect(
+      307,
+      "https://" + req.hostname + ":" + app.get("secPort") + req.url
+    );
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
